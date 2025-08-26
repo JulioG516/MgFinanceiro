7a12e9ff916cee862209bbd4d9667984ce9c5b93
Julio Gabriel <juliogabriel516@gmail.com>
2025-08-26 17:17:01 -0300

    Trocado os  Models para o Relatorio

---
+++ MgFinanceiro.Domain/Entities/RelatorioPorCategoria.cs
@@ -0,0 +1,11 @@
+﻿namespace MgFinanceiro.Domain.Entities;
+
+public class RelatorioPorCategoria
+{
+    public int Ano { get; set; }
+    public int Mes { get; set; }
+    public int CategoriaId { get; set; }
+    public string CategoriaNome { get; set; } = string.Empty;
+    public TipoCategoria CategoriaTipo { get; set; }
+    public decimal Total { get; set; }
+}
---
+++ MgFinanceiro.Domain/Entities/RelatorioResumo.cs
@@ -0,0 +1,10 @@
+﻿namespace MgFinanceiro.Domain.Entities;
+
+public class RelatorioResumo
+{
+    public int Ano { get; set; }
+    public int Mes { get; set; }
+    public decimal SaldoTotal { get; set; }
+    public decimal TotalReceitas { get; set; }
+    public decimal TotalDespesas { get; set; }
+}
