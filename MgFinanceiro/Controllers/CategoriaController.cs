bdf0b6d4dca8ccf41eebe0fb8797d5cf850e287d
Julio Gabriel <juliogabriel516@gmail.com>
2025-08-26 12:59:19 -0300

    Adicionado controller e configurado DI

---
+++ MgFinanceiro/Controllers/CategoriaController.cs
@@ -0,0 +1,38 @@
+﻿using MgFinanceiro.Application.DTOs;
+using MgFinanceiro.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MgFinanceiro.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriaController : ControllerBase
+{
+    private readonly ICategoriaService _categoriaService;
+
+    public CategoriaController(ICategoriaService categoriaService)
+    {
+        _categoriaService = categoriaService;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<CategoriaResponseDto>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAll([FromQuery] int? tipoCategoria)
+    {
+        var categorias = await _categoriaService.GetAllCategoriasAsync(tipoCategoria);
+        return Ok(categorias);
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateCategoriaRequest request)
+    {
+        var result = await _categoriaService.CreateCategoriaAsync(request);
+        if (!result.IsSuccess)
+        {
+            return BadRequest(result.Error);
+        }
+
+        return CreatedAtAction(nameof(GetAll), null, request);
+    }
+}
--- MgFinanceiro/Program.cs
+++ MgFinanceiro/Program.cs
@@ -1,5 +1,13 @@
+using FluentValidation;
+using MgFinanceiro.Application.DTOs;
+using MgFinanceiro.Application.Interfaces;
+using MgFinanceiro.Application.Services;
+using MgFinanceiro.Application.Validators;
+using MgFinanceiro.Domain.Interfaces;
 using MgFinanceiro.Infrastructure.Data;
+using MgFinanceiro.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
+using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,10 +18,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Validators
+
+// Validator direto no controller
+builder.Services.AddFluentValidationAutoValidation();
+
+builder.Services.AddScoped<IValidator<CreateCategoriaRequest>, CreateCategoriaRequestValidator>();
+
 // Db
 builder.Services.AddDbContext<AppDbContext>(options
     => options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));
 
+
+// DI
+builder.Services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+builder.Services.AddScoped<ICategoriaService, CategoriaService>();
 
 var app = builder.Build();
 
