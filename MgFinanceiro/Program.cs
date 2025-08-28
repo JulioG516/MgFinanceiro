28724c8fdff10d714352d7a7d5fba552f506edb0
= <juliogabriel516@gmail.com>
2025-08-28 00:15:24 -0300

    Add dependencies for JWT and Swagger config

--- MgFinanceiro/Program.cs
+++ MgFinanceiro/Program.cs
@@ -1,4 +1,6 @@
+using System.Text;
 using FluentValidation;
+using MgFinanceiro.Application.DTOs.Auth;
 using MgFinanceiro.Application.DTOs.Categoria;
 using MgFinanceiro.Application.DTOs.Relatorio;
 using MgFinanceiro.Application.DTOs.Transacao;
@@ -8,7 +10,9 @@ using MgFinanceiro.Application.Validators;
 using MgFinanceiro.Domain.Interfaces;
 using MgFinanceiro.Infrastructure.Data;
 using MgFinanceiro.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
 using Swashbuckle.AspNetCore.SwaggerGen;
@@ -18,7 +22,41 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+var secretKey = jwtSettings["SecretKey"];
+
+if (string.IsNullOrEmpty(secretKey))
+{
+    throw new InvalidOperationException("JWT SecretKey não esta configurada no appsettings.json");
+}
+
+var key = Encoding.UTF8.GetBytes(secretKey);
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings["Issuer"],
+            ValidAudience = jwtSettings["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ClockSkew = TimeSpan.Zero // Remove delay de 5 minutos padrão
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(opt =>
 {
@@ -41,6 +79,37 @@ builder.Services.AddSwaggerGen(opt =>
             Email = "juliogabriel516@gmail.com"
         }
     });
+    
+    
+    // Configuração JWT para Swagger
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header usando Bearer scheme. 
+                      Digite 'Bearer' [espaço] e então seu token.
+                      Exemplo: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
 });
 
 
@@ -54,6 +123,8 @@ builder.Services.AddScoped<IValidator<CreateCategoriaRequest>, CreateCategoriaRe
 builder.Services.AddScoped<IValidator<CreateTransacaoRequest>, CreateTransacaoRequestValidator>();
 builder.Services.AddScoped<IValidator<UpdateTransacaoRequest>, UpdateTransacaoRequestValidator>();
 builder.Services.AddScoped<IValidator<RelatorioQueryDto>, RelatorioQueryValidator>();
+builder.Services.AddScoped<IValidator<LoginRequest>, LoginRequestValidator>();
+builder.Services.AddScoped<IValidator<UsuarioRegisterRequest>, UsuarioRegisterRequestValidator>();
 
 // Db
 builder.Services.AddDbContext<AppDbContext>(options
@@ -67,6 +138,8 @@ builder.Services.AddScoped<ITransacaoRepository, TransacaoRepository>();
 builder.Services.AddScoped<ITransacaoService, TransacaoService>();
 builder.Services.AddScoped<IRelatorioRepository, RelatorioRepository>();
 builder.Services.AddScoped<IRelatorioService, RelatorioService>();
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 
 var app = builder.Build();
