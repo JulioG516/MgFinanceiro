0bd5349d810c7a87ca71e42943bc386dc1166728
= <juliogabriel516@gmail.com>
2025-08-28 00:13:42 -0300

    Add Auth Validators

---
+++ MgFinanceiro.Application/Validators/LoginRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using MgFinanceiro.Application.DTOs.Auth;
+
+namespace MgFinanceiro.Application.Validators;
+
+public class LoginRequestValidator:  AbstractValidator<LoginRequest>
+{
+    public LoginRequestValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email é obrigatório")
+            .EmailAddress().WithMessage("Email deve ter formato válido");
+
+        RuleFor(x => x.Senha)
+            .NotEmpty().WithMessage("Senha é obrigatória")
+            .MinimumLength(6).WithMessage("Senha deve ter pelo menos 6 caracteres")
+            .MaximumLength(100).WithMessage("Senha deve ter no máximo 100 caracteres");
+    }
+}
---
+++ MgFinanceiro.Application/Validators/UsuarioRegisterRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using MgFinanceiro.Application.DTOs.Auth;
+
+namespace MgFinanceiro.Application.Validators;
+
+public class UsuarioRegisterRequestValidator : AbstractValidator<UsuarioRegisterRequest>
+{
+    public UsuarioRegisterRequestValidator()
+    {
+        RuleFor(x => x.Nome)
+            .NotEmpty().WithMessage("Nome é obrigatório")
+            .MaximumLength(100).WithMessage("Nome deve ter no máximo 100 caracteres");
+        
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email é obrigatório.")
+            .EmailAddress().WithMessage("Deve ser fornecido um email válido.");
+
+        RuleFor(x => x.Senha)
+            .MinimumLength(6).WithMessage("Senha deve ter pelo menos 6 caracteres")
+            .MaximumLength(100).WithMessage("Senha deve ter no máximo 100 caracteres");
+    }
+}
