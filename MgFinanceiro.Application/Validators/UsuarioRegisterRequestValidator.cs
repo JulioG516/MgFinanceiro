730ab732d9729712c4e68527f39c80bd07890a52
= <juliogabriel516@gmail.com>
2025-08-28 00:43:40 -0300

    Add Usuario Register Validator Cosntraints

--- MgFinanceiro.Application/Validators/UsuarioRegisterRequestValidator.cs
+++ MgFinanceiro.Application/Validators/UsuarioRegisterRequestValidator.cs
@@ -10,10 +10,11 @@ public class UsuarioRegisterRequestValidator : AbstractValidator<UsuarioRegister
         RuleFor(x => x.Nome)
             .NotEmpty().WithMessage("Nome é obrigatório")
             .MaximumLength(100).WithMessage("Nome deve ter no máximo 100 caracteres");
-        
+
         RuleFor(x => x.Email)
             .NotEmpty().WithMessage("Email é obrigatório.")
-            .EmailAddress().WithMessage("Deve ser fornecido um email válido.");
+            .EmailAddress().WithMessage("Deve ser fornecido um email válido.")
+            .MaximumLength(255).WithMessage("Email deve ter no maximo 255 caracteres");
 
         RuleFor(x => x.Senha)
             .MinimumLength(6).WithMessage("Senha deve ter pelo menos 6 caracteres")
