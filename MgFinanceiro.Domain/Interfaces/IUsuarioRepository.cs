0b937656dcdcfb7ae0eab0ed2a2981c4765f6b98
= <juliogabriel516@gmail.com>
2025-08-27 23:32:17 -0300

    Add IUsuarioRepository

---
+++ MgFinanceiro.Domain/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,12 @@
+﻿using MgFinanceiro.Domain.Entities;
+
+namespace MgFinanceiro.Domain.Interfaces;
+
+public interface IUsuarioRepository
+{
+    Task<Usuario?> GetByEmailAsync(string email);
+    Task<Usuario?> GetByIdAsync(int id);
+    Task<Usuario> CreateAsync(Usuario usuario);
+    Task UpdateLastLoginAsync(int usuarioId);
+    Task<bool> EmailExistsAsync(string email);
+}
