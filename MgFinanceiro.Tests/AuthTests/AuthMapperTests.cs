7ac6cab1a962aa6d94f4fc478c1dfff0762f60fe
Julio Gabriel <juliogabriel516@gmail.com>
2025-08-28 08:40:04 -0300

    Add AuthMapperTests

---
+++ MgFinanceiro.Tests/AuthTests/AuthMapperTests.cs
@@ -0,0 +1,32 @@
+﻿using MgFinanceiro.Application.Mappers;
+using MgFinanceiro.Domain.Entities;
+using Shouldly;
+
+namespace MgFinanceiro.Tests.AuthTests;
+
+public class AuthMapperTests
+{
+    [Fact]
+    private void Should_Map_Correctly_To_Usuario_Entity_To_Dto()
+    {
+        var createdAt = DateTime.Now;
+
+        var usuario = new Usuario
+        {
+            Id = 1,
+            Nome = "Usuario Teste",
+            Email = "teste@exemplo.com",
+            SenhaHash = "hashDeUmaSenha",
+            DataCriacao = createdAt,
+            UltimoLogin = createdAt.AddDays(-1),
+        };
+
+        var dto = AuthMapper.UsuarioToDto(usuario);
+        dto.ShouldNotBeNull();
+        dto.Id.ShouldBe(1);
+        dto.Nome.ShouldBe("Usuario Teste");
+        dto.Email.ShouldBe("teste@exemplo.com");
+        dto.DataCriacao.ShouldBe(createdAt);
+        dto.UltimoLogin.ShouldBe(createdAt.AddDays(-1));
+    }
+}
