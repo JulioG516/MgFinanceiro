0db3a8ef23d66a543befef7bfbc50e1f8a00dc90
Julio Gabriel <juliogabriel516@gmail.com>
2025-08-28 11:50:37 -0300

    Fix Init Sql script

--- init.sql
+++ init.sql
@@ -11,8 +11,7 @@ GO
 USE MgFinanceiroDb;
 GO
 
-BEGIN
-TRY
+BEGIN TRY
 BEGIN TRANSACTION;
 
 IF OBJECT_ID(N'[FK_Transacoes_Categorias_CategoriaId]', 'F') IS NOT NULL
@@ -30,108 +29,40 @@ DROP TABLE [Usuarios];
 IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NOT NULL
 DROP TABLE [__EFMigrationsHistory];
 
-CREATE TABLE [__EFMigrationsHistory]
-(
-    [
-    MigrationId]
-    nvarchar
-(
-    150
-) NOT NULL,
-    [ProductVersion] nvarchar
-(
-    32
-) NOT NULL,
-    CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY
-(
-[
-    MigrationId]
-)
+CREATE TABLE [__EFMigrationsHistory] (
+    [MigrationId] nvarchar(150) NOT NULL,
+    [ProductVersion] nvarchar(32) NOT NULL,
+    CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
     );
 
-CREATE TABLE [Categorias]
-(
-    [
-    Id]
-    int
-    NOT
-    NULL
-    IDENTITY, [
-    Nome]
-    nvarchar
-(
-    100
-) NOT NULL,
+CREATE TABLE [Categorias] (
+    [Id] int NOT NULL IDENTITY,
+    [Nome] nvarchar(100) NOT NULL,
     [Tipo] int NOT NULL,
     [Ativo] bit NOT NULL,
     [DataCriacao] datetime2 NOT NULL,
-    CONSTRAINT [PK_Categorias] PRIMARY KEY
-(
-[
-    Id]
-)
+    CONSTRAINT [PK_Categorias] PRIMARY KEY ([Id])
     );
 
-CREATE TABLE [Transacoes]
-(
-    [
-    Id]
-    int
-    NOT
-    NULL
-    IDENTITY, [
-    Descricao]
-    nvarchar
-(
-    200
-) NOT NULL,
-    [Valor] decimal
-(
-    18,
-    2
-) NOT NULL,
+CREATE TABLE [Transacoes] (
+    [Id] int NOT NULL IDENTITY,
+    [Descricao] nvarchar(200) NOT NULL,
+    [Valor] decimal(18,2) NOT NULL,
     [Data] datetime2 NOT NULL,
     [CategoriaId] int NOT NULL,
-    [Observacoes] nvarchar
-(
-    500
-) NULL,
+    [Observacoes] nvarchar(500) NULL,
     [DataCriacao] datetime2 NOT NULL,
-    CONSTRAINT [PK_Transacoes] PRIMARY KEY
-(
-[
-    Id]
-)
+    CONSTRAINT [PK_Transacoes] PRIMARY KEY ([Id])
     );
 
-CREATE TABLE [Usuarios]
-(
-    [
-    Id]
-    int
-    NOT
-    NULL
-    IDENTITY, [
-    Nome]
-    nvarchar
-(
-    100
-) NOT NULL,
-    [Email] nvarchar
-(
-    100
-) NOT NULL,
-    [SenhaHash] nvarchar
-(
-    max
-) NOT NULL,
+CREATE TABLE [Usuarios] (
+    [Id] int NOT NULL IDENTITY,
+    [Nome] nvarchar(100) NOT NULL,
+    [Email] nvarchar(100) NOT NULL,
+    [SenhaHash] nvarchar(max) NOT NULL,
     [DataCriacao] datetime2 NOT NULL,
     [UltimoLogin] datetime2 NULL,
-    CONSTRAINT [PK_Usuarios] PRIMARY KEY
-(
-[
-    Id]
-)
+    CONSTRAINT [PK_Usuarios] PRIMARY KEY ([Id])
     );
 
 ALTER TABLE [Transacoes] ADD CONSTRAINT [FK_Transacoes_Categorias_CategoriaId] FOREIGN KEY ([CategoriaId]) REFERENCES [Categorias] ([Id]) ON DELETE CASCADE;
@@ -152,46 +83,26 @@ SET IDENTITY_INSERT [Categorias] OFF;
 
 SET IDENTITY_INSERT [Transacoes] ON;
 INSERT INTO [Transacoes] ([Id], [CategoriaId], [Data], [DataCriacao], [Descricao], [Observacoes], [Valor])
-VALUES (1, 1, '2025-08-23T00:00:00.0000000Z', '2025-08-28T14:24:46.4441287Z', N'Venda de produtos para cliente A',
-        N'Venda de 50 unidades de produto X, entrega realizada em 2 dias.', 1500.0),
-       (2, 2, '2025-08-25T00:00:00.0000000Z', '2025-08-28T14:24:46.4441808Z', N'Prestação de serviço de consultoria',
-        N'Consultoria de 10 horas para otimização de processos.', 800.0),
-       (3, 5, '2025-08-18T00:00:00.0000000Z', '2025-08-28T14:24:46.4441811Z', N'Pagamento de salários mensais',
-        N'Salários de 5 funcionários, pago via transferência bancária.', 2000.0),
-       (4, 7, '2025-08-26T00:00:00.0000000Z', '2025-08-28T14:24:46.4441812Z', N'Compra de estoque',
-        N'Aquisição de 100 unidades de matéria-prima Y.', 1200.0),
-       (5, 4, '2025-07-10T00:00:00.0000000Z', '2025-08-28T14:24:46.4441813Z', N'Receita de aluguel de equipamento',
-        N'Aluguel de máquina por 5 dias para cliente externo.', 600.0),
-       (6, 6, '2025-07-05T00:00:00.0000000Z', '2025-08-28T14:24:46.4441814Z', N'Pagamento de aluguel mensal', NULL,
-        900.0),
-       (7, 1, '2025-09-12T00:00:00.0000000Z', '2025-08-28T14:24:46.4441815Z', N'Venda de produtos para cliente B',
-        N'Venda de 80 unidades de produto Z, pedido urgente.', 2300.0),
-       (8, 9, '2025-09-15T00:00:00.0000000Z', '2025-08-28T14:24:46.4441817Z', N'Campanha de marketing online',
-        N'Anúncios no Google Ads para promoção de produto X.', 500.0),
-       (9, 3, '2025-08-30T00:00:00.0000000Z', '2025-08-28T14:24:46.4441826Z', N'Juros recebidos de aplicação',
-        N'Juros de aplicação financeira em fundo de renda fixa.', 300.0),
-       (10, 8, '2025-07-20T00:00:00.0000000Z', '2025-08-28T14:24:46.4441827Z', N'Pagamento de impostos municipais',
-        NULL, 400.0),
-       (11, 6, '2025-06-25T00:00:00.0000000Z', '2025-08-28T14:24:46.4441828Z', N'Serviço de manutenção de equipamentos',
-        N'Manutenção preventiva de 3 máquinas industriais.', 700.0),
-       (12, 1, '2025-06-15T00:00:00.0000000Z', '2025-08-28T14:24:46.4441830Z', N'Venda de produtos para cliente C',
-        N'Venda de 60 unidades de produto Y, pagamento à vista.', 1800.0),
-       (13, 10, '2025-08-10T00:00:00.0000000Z', '2025-08-28T14:24:46.4441831Z', N'Despesas com material de escritório',
-        N'Compra de papel, canetas e outros suprimentos.', 200.0),
-       (14, 2, '2025-09-05T00:00:00.0000000Z', '2025-08-28T14:24:46.4441832Z', N'Receita de consultoria estratégica',
-        NULL, 1200.0),
-       (15, 7, '2025-07-28T00:00:00.0000000Z', '2025-08-28T14:24:46.4441833Z', N'Compra de matérias-primas',
-        N'Aquisição de matéria-prima para produção de produto Z.', 1500.0),
-       (16, 8, '2025-06-30T00:00:00.0000000Z', '2025-08-28T14:24:46.4441834Z', N'Pagamento de taxas estaduais',
-        N'ICMS referente a vendas do mês de junho.', 350.0),
-       (17, 4, '2025-08-05T00:00:00.0000000Z', '2025-08-28T14:24:46.4441835Z', N'Receita de royalties',
-        N'Royalties de licenciamento de software proprietário.', 1000.0),
-       (18, 6, '2025-09-20T00:00:00.0000000Z', '2025-08-28T14:24:46.4441837Z', N'Manutenção de veículos da empresa',
-        NULL, 600.0),
-       (19, 1, '2025-07-15T00:00:00.0000000Z', '2025-08-28T14:24:46.4441838Z', N'Venda de produtos para cliente D',
-        N'Venda de 100 unidades de produto X, entrega em 3 dias.', 2500.0),
-       (20, 10, '2025-08-12T00:00:00.0000000Z', '2025-08-28T14:24:46.4441839Z', N'Despesas com treinamento de equipe',
-        NULL, 800.0);
+VALUES (1, 1, '2025-08-23T00:00:00.0000000Z', '2025-08-28T13:40:52.4186124Z', N'Venda de produtos para cliente A', NULL, 1500.0),
+       (2, 2, '2025-08-25T00:00:00.0000000Z', '2025-08-28T13:40:52.4186581Z', N'Prestação de serviço de consultoria', NULL, 800.0),
+       (3, 5, '2025-08-18T00:00:00.0000000Z', '2025-08-28T13:40:52.4186583Z', N'Pagamento de salários mensais', NULL, 2000.0),
+       (4, 7, '2025-08-26T00:00:00.0000000Z', '2025-08-28T13:40:52.4186584Z', N'Compra de estoque', NULL, 1200.0),
+       (5, 4, '2025-07-10T00:00:00.0000000Z', '2025-08-28T14:15:33.2602063Z', N'Receita de aluguel de equipamento', NULL, 600.0),
+       (6, 6, '2025-07-05T00:00:00.0000000Z', '2025-08-28T14:15:33.2602064Z', N'Pagamento de aluguel mensal', NULL, 900.0),
+       (7, 1, '2025-09-12T00:00:00.0000000Z', '2025-08-28T14:15:33.2602065Z', N'Venda de produtos para cliente B', NULL, 2300.0),
+       (8, 9, '2025-09-15T00:00:00.0000000Z', '2025-08-28T14:15:33.2602066Z', N'Campanha de marketing online', NULL, 500.0),
+       (9, 3, '2025-08-30T00:00:00.0000000Z', '2025-08-28T14:15:33.2602067Z', N'Juros recebidos de aplicação', NULL, 300.0),
+       (10, 8, '2025-07-20T00:00:00.0000000Z', '2025-08-28T14:15:33.2602078Z', N'Pagamento de impostos municipais', NULL, 400.0),
+       (11, 6, '2025-06-25T00:00:00.0000000Z', '2025-08-28T14:15:33.2602079Z', N'Serviço de manutenção de equipamentos', NULL, 700.0),
+       (12, 1, '2025-06-15T00:00:00.0000000Z', '2025-08-28T14:15:33.2602080Z', N'Venda de produtos para cliente C', NULL, 1800.0),
+       (13, 10, '2025-08-10T00:00:00.0000000Z', '2025-08-28T14:15:33.2602081Z', N'Despesas com material de escritório', NULL, 200.0),
+       (14, 2, '2025-09-05T00:00:00.0000000Z', '2025-08-28T14:15:33.2602082Z', N'Receita de consultoria estratégica', NULL, 1200.0),
+       (15, 7, '2025-07-28T00:00:00.0000000Z', '2025-08-28T14:15:33.2602083Z', N'Compra de matérias-primas', NULL, 1500.0),
+       (16, 8, '2025-06-30T00:00:00.0000000Z', '2025-08-28T14:15:33.2602084Z', N'Pagamento de taxas estaduais', NULL, 350.0),
+       (17, 4, '2025-08-05T00:00:00.0000000Z', '2025-08-28T14:15:33.2602085Z', N'Receita de royalties', NULL, 1000.0),
+       (18, 6, '2025-09-20T00:00:00.0000000Z', '2025-08-28T14:15:33.2602086Z', N'Manutenção de veículos da empresa', NULL, 600.0),
+       (19, 1, '2025-07-15T00:00:00.0000000Z', '2025-08-28T14:15:33.2602087Z', N'Venda de produtos para cliente D', NULL, 2500.0),
+       (20, 10, '2025-08-12T00:00:00.0000000Z', '2025-08-28T14:15:33.2602089Z', N'Despesas com treinamento de equipe', NULL, 800.0);
 SET IDENTITY_INSERT [Transacoes] OFF;
 
 SET IDENTITY_INSERT [Usuarios] ON;
@@ -223,8 +134,7 @@ WHERE NOT EXISTS (SELECT 1 FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'
 
 COMMIT TRANSACTION;
 END TRY
-BEGIN
-CATCH
+BEGIN CATCH
     IF @@TRANCOUNT > 0
 ROLLBACK TRANSACTION;
 
