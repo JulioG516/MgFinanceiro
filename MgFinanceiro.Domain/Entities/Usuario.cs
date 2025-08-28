370a32f2afabec8be93260ab506c85c4157ed105
= <juliogabriel516@gmail.com>
2025-08-27 23:32:03 -0300

    Add Usuario Entity

---
+++ MgFinanceiro.Domain/Entities/Usuario.cs
@@ -0,0 +1,15 @@
+﻿namespace MgFinanceiro.Domain.Entities;
+
+public class Usuario
+{
+    public int Id { get; set; }
+        
+    public string Nome { get; set; } = string.Empty;
+        
+    public string Email { get; set; } = string.Empty;
+        
+    public string SenhaHash { get; set; } = string.Empty;
+    
+    public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
+    public DateTime? UltimoLogin { get; set; }
+}
