ef357f02de36967340bfe5922f72cac5cd683843
= <juliogabriel516@gmail.com>
2025-08-28 00:13:35 -0300

    Add AuthMapper

---
+++ MgFinanceiro.Application/Mappers/AuthMapper.cs
@@ -0,0 +1,12 @@
+﻿using MgFinanceiro.Application.DTOs.Auth;
+using MgFinanceiro.Domain.Entities;
+using Riok.Mapperly.Abstractions;
+
+namespace MgFinanceiro.Application.Mappers;
+
+[Mapper(RequiredMappingStrategy = RequiredMappingStrategy.Source)]
+public partial class AuthMapper
+{
+    [MapperIgnoreSource(nameof(Usuario.SenhaHash))]
+    public static partial UsuarioDto UsuarioToDto(Usuario usuario);
+}
