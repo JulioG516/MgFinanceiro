b4cda4524f4f108a3c4f98f0fb31d928f3d1e102
= <juliogabriel516@gmail.com>
2025-08-29 23:13:54 -0300

    Add Models used in changes categoria service

---
+++ MgFinanceiro.Application/DTOs/Categoria/UpdateCategoriaStatusRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace MgFinanceiro.Application.DTOs.Categoria;
+
+public class UpdateCategoriaStatusRequest
+{
+    public int Id { get; set; }
+    public bool Ativo { get; set; }
+}
---
+++ MgFinanceiro.Application/Validators/UpdateCategoriaRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using MgFinanceiro.Application.DTOs.Categoria;
+
+namespace MgFinanceiro.Application.Validators;
+
+public class UpdateCategoriaRequestValidator : AbstractValidator<UpdateCategoriaStatusRequest>
+{
+    public UpdateCategoriaRequestValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Id deve ser fornecido");
+
+        RuleFor(x => x.Ativo)
+            .NotNull().WithMessage("Ativo deve ser fornecido");
+    }
+}
